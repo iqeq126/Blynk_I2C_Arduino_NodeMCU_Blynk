063fd249bdbbdad0a575d1cfe7f98539b732b08d
iqeq126 <108468000+iqeq126@users.noreply.github.com>
2023-12-01 20:44:53 +0900

    재이용수를 활용한 야외시설 냉각장치
    동국대 물시티 공모전 구현 자료

--- arduino_uno.ino
+++ arduino_uno.ino
@@ -1,10 +1,13 @@
 // Arduino 
 // LCD header
-#include <LiquidCrystal.h>
+#include <LiquidCrystal_I2C.h>
+#include <Servo.h>
+Servo myservo;
+LiquidCrystal_I2C lcd(0x27, 16, 2);  //이상하면 0x3F,16,2로 바꾸기
 
 // SoftwareSerial header
-#include <SoftwareSerial.h>
-#include <Wire.h>
+//#include <SoftwareSerial.h>
+//#include <Wire.h>
 
 //DHT header
 #include <DHT.h>
@@ -16,96 +19,88 @@
 //DHT 설정
 DHT dht(DHTPIN, DHTTYPE);  // DHT설정 - dht (디지털2, dht11)
 
-int pinWATER_MOTOR1 = 3;  //저류지모터 : 3번핀
-int pinWATER_MOTOR2 = 4;  //정류장모터(의자) : 4번핀
-
-int pinWATER_MOTOR3 = 5;  //정류장모터(벽) : 5번핀
-int pinTDS_in = 6;        //tds센서 : 6번핀
+const int pinWATER_MOTOR1 = 3;  //저류지모터 : 3번핀
+const int pinWATER_MOTOR2 = 4;  //정류장모터(의자) : 4번핀
+const int pinWATER_MOTOR3 = 5;  //정류장모터(벽) : 5번핀
+const int pinTDS_in = A0;        //tds센서 : 6번핀
+const int ENABLE=11;            // 모터1사용 가능 제어핀과 동시에 속도제어핀 역할
+const int DIR1=10;               // 방향제어를 위한 핀1
+const int DIR2=9;               // 방향제어를 위한 핀2
+const int BUTTON = 3;           // 버튼입력으로 사용할 핀
+const int POT = A5;             // 가변저항기와 연결한 핀
+enum MOTOR_DIRECTION{FOWARD=0, BACKWARD=1};         // 모터의 전방, 후방 여부 확인을 위한 자료형 
+  
+enum MOTOR_DIRECTION Direction;                     // 모터의 전방, 후방 여부 확인을 위한 자료형 생성
 
 //lcd
-LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+//LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 
-int temp = 1;
-int humi = 1;
-
+float temp = 0.0;
+float humi = 0.0;
+int tdsValue = 0;
+float voltage = 0.0;
 void setup() {
   Serial.begin(9600);
-  //soft.begin(9600);  
-   //Wire.begin(8);                /* join i2c bus with address 8 */
-   //Wire.onReceive(receiveEvent); /* register receive event */
-   //Wire.onRequest(requestEvent); /* register request event */
-
   delay(100);
-  lcd.begin(20, 4);
+  lcd.init();       // Initialize the LCD
+  lcd.backlight();  // Turn on the backlight
   lcd.setCursor(3, 1);
   // put your setup code here, to run once:
   pinMode(pinWATER_MOTOR1, OUTPUT);  //저류지 모터
   pinMode(pinWATER_MOTOR2, OUTPUT);  //정류장 모터
+  myservo.attach(pinWATER_MOTOR3);
 
   pinMode(DHTPIN, INPUT);     // 온습도 센서 핀모드
   pinMode(pinTDS_in, INPUT);  // TDS 센서 핀모드
 
+  pinMode(ENABLE, OUTPUT);             // 모터1사용 가능 제어핀 출력으로 설정
+  pinMode(DIR1, OUTPUT);               // 방향제어를 위한 핀1 출력으로 설정
+  pinMode(DIR2, OUTPUT);              // 방향제어를 위한 핀2 출력으로 설정
+  pinMode(BUTTON, INPUT);             // 버튼핀 입력으로 설정
+  digitalWrite(ENABLE, LOW);         // 일단 모터가 작동하지 않도록 초기화
 
-  //Serial.println("\nI2C Scanner");
-  //Serial.println("DHTxx test!");
   dht.begin();
 }
 
 void loop() {
 
   //TDS 센서
-  int tdsValue = digitalRead(pinTDS_in);
-  //Serial.println(tdsValue);
-  //delay(500);
+  tdsValue = analogRead(pinTDS_in);
+  voltage = tdsValue * 5.0 / 1024.0;
+  Serial.println(voltage);
+
   //DHT
+  
   temp = dht.readTemperature();  //온습도 온도 값 저장 변수
   humi = dht.readHumidity();
-  //soft.println(String(temp));
-  //soft.println(String(humi));
+  delay(1000);
+    lcd.clear();  // Clear the display
+  lcd.setCursor(0, 0);
+  lcd.print("temp: ");
+  lcd.setCursor(8, 0);
+  lcd.print(float(temp));
+  lcd.setCursor(15, 0);
+  lcd.print("C");
+
+  Serial.println(tdsValue);
   Serial.println(temp);
   Serial.println(humi);
-  delay(1000);
-  //delay(500);
   //온도와 tds 센서로 저류지 모터 on/off
-  if (temp >= 20) {
-    if (tdsValue == HIGH) {  //Read sensor signal
-      analogWrite(3, 255);   // 펌프모터 켜기
+  if (temp >= 25) {
+    if (voltage >= 3.30) {  //Read sensor signal
+      analogWrite(pinWATER_MOTOR1, 255);   // 펌프모터 켜기
+      analogWrite(pinWATER_MOTOR2, 255);  // 펌프모터 켜기
+      myservo.write(180);   //Counterclockwise maximum speed rotation
+      Serial.println(1);
+      Serial.println(1);
       //Serial.println("first pump water on");
     } else {
-      analogWrite(3, 0);  // 펌프모터 끄기
+      analogWrite(pinWATER_MOTOR1, 0);  // 펌프모터 끄기
+      analogWrite(pinWATER_MOTOR2, 0);  // 펌프모터 끄기
+      myservo.write(90);  //Counterclockwise maximum speed rotation
+      Serial.println(0);
+      Serial.println(0);
       //Serial.println("first pump water off");
     }
   }
-
-  //정류장(벽, 의자)
-  if (tdsValue == HIGH) {  //Read sensor signal
-    analogWrite(4, 255);   // 펌프모터 켜기
-    analogWrite(5, 255);   // 펌프모터 켜기
-    //Serial.println("second pump water on");
-  } else {
-    analogWrite(4, 0);  // 펌프모터 끄기
-    analogWrite(5, 0);  // 펌프모터 켜기
-    //Serial.println("second pump water off");
-  }
 }
-/*
-void receiveEvent(int howMany) {
- while (0 <Wire.available()) {
-    char c = Wire.read();      // receive byte as a character 
-    Serial.print(c);           // print the character 
-  }
- Serial.println();             // to newline 
-}
-*/
-// function that executes whenever data is requested from master
-/*void requestEvent() {
- if ( Wire.available() >= 0){
-    Serial.println(temp);
-    Serial.println(humi);
-    Wire.beginTransmission(8);  // Transmit to NodeMCU with address 8
-    Wire.write(temp);  // Send data
-    Wire.write(humi);  // Send data
-    delay(500);
-    Wire.endTransmission();   // Stop transmitting
-  }
-}*/
--- nodemcu.ino
+++ nodemcu.ino
@@ -4,9 +4,9 @@
 // #define BlynkTimer timer; // Announcing the timer
 
 /* Fill-in your Template ID (only if using Blynk.Cloud) */
-#define BLYNK_TEMPLATE_ID "YourID"
-#define BLYNK_TEMPLATE_NAME "YourTemplate"
-#define BLYNK_AUTH_TOKEN "YourToken"
+#define BLYNK_TEMPLATE_ID "TMPL6KdbIgawS"
+#define BLYNK_TEMPLATE_NAME "BusstopCooler2"
+#define BLYNK_AUTH_TOKEN "bz-yFhnAm7QWDK443r-AURpzxpDOv1JW"
 
 // Blynk && Serial 
 #include <ESP8266WiFi.h>
@@ -14,23 +14,30 @@
 #include <SoftwareSerial.h>
 #include <DHT.h>
 char auth[] = BLYNK_AUTH_TOKEN;
-char ssid[] = "YourID"; // WIFI ID
-char pass[] = "YourPW"; // WIFI PASSWOARD
+char ssid[] = "1"; // WIFI ID
+char pass[] = "qhdks001!"; // WIFI PASSWOARD
 
 //pin번호 정의
 int tempPin = D4;
 int humiPin = D5;
 // 변수 정의
-int temp;
-int humi;
+float temp;
+float humi;
+int tdsValue;
+int motor1;
+int motor2;
 
 // push를 위한 타이머 인스턴스
 BlynkTimer timer;
 // 1초마다 Blynk 서버에 정보 전달
 void myTimerEvent()
 {
+    float Discomfort = 1.8 * temp - 0.55 * (1 - humi * 0.01) * (1.8 * temp - 26) + 32;
     Blynk.virtualWrite(V0, temp);
     Blynk.virtualWrite(V1, humi);
+    Blynk.virtualWrite(V2, Discomfort);
+    Blynk.virtualWrite(V3, motor1);
+    Blynk.virtualWrite(V4, motor2);
 }
 
 void setup()
@@ -40,7 +47,7 @@ void setup()
   pinMode(tempPin, OUTPUT);
   pinMode(humiPin, OUTPUT);
   Blynk.begin(auth, ssid, pass);
-  
+
   timer.setInterval(1000L, myTimerEvent);
 }
 
@@ -48,7 +55,10 @@ void loop()
 {
   Blynk.run();
   timer.run();
-  temp = Serial.readStringUntil('\n').toInt();
-  humi = Serial.readStringUntil('\n').toInt();
-  delay(100);
+  tdsValue = Serial.readStringUntil('\n').toInt();
+  temp = Serial.readStringUntil('\n').toFloat();
+  humi = Serial.readStringUntil('\n').toFloat();
+  motor1 = Serial.readStringUntil('\n').toInt();
+  motor2 = Serial.readStringUntil('\n').toInt();
+  delay(200);
 }
